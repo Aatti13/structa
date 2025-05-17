fef1b955ed31bf2cef635e7922365ce75ac22b78
Aatti13 <86865554+Aatti13@users.noreply.github.com>
2025-05-17 14:06:21 +0530

    PURGE: Removed apps folder Restarting from scratch

--- apps/api/src/config/jwt.js
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-export const generateToken = (payload, expiresIn = '1d') => {
-  return jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn });
-}
-
-export const resetToken = (payload, expiresIn = '7d') => {
-  return jwt.sign(payload, process.env.JWT_RESET_SECRET, { expiresIn });
-}

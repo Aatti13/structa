de4c4f3916c85e5ff58ea60c03aff50bc9bad25a
Aatti13 <86865554+Aatti13@users.noreply.github.com>
2025-06-01 14:49:16 +0530

    completed UI for sign-up page

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 
+/**
+ * 
+ * @returns value={registrationData.password} onChange={(e)=>setSignupdata({...signUpdata, password: e.target.value})} required
+ * value={registrationData.username} onChange={(e)=> setSignupdata({...signUpdata, username: e.target.value})}
+ * value={registrationData.email} onChange={(e)=>setSignupdata({...signUpdata, email: e.target.value})}
+ */
+
 const RegisterPage = () => {
 
   const handeRegistration = (e)=>{
@@ -8,11 +15,11 @@ const RegisterPage = () => {
 
   return (
     <div className='h-screen flex justify-center items-center' data-theme="dracula">
-      <div className="signup-card bg-black-clr flex justify-center items-center rounded-3xl h-140 w-3xl">
+      <div className="signup-card bg-black-clr flex justify-center items-center rounded-3xl h-160 w-3xl">
         <div className='flex flex-col h-140 rounded w-2/4 b-r-left'>
           <figure className='flex items-center'>
-            <img src="/images/structa logo.png" alt="logo" className='w-15 mt-9 ml-10'/>
-            <span className='text-3xl mt-9'>
+            <img src="/images/structa logo.png" alt="logo" className='w-15 mt-6 ml-10'/>
+            <span className='text-3xl mt-6'>
               Structa
             </span>
           </figure>
@@ -25,34 +32,46 @@ const RegisterPage = () => {
                 </div>
                 <div className="space-y-3">
                   <div className="form-control w-full flex flex-col">
-                    <label  className="label ml-12 caret-green-500 placeholder:ml-[5px]">
+                    <label  className="text-xs label ml-14 caret-green-500 placeholder:ml-[5px]">
                       Username
                     </label>
-                    <input type="text" value={signUpdata.username} onChange={(e)=> setSignupdata({...signUpdata, username: e.target.value})} required placeholder='Enter Username' className=' text-input ml-12 mt-1 w-60 h-10 outline-sky-50'/>
+                    <input type="text" required placeholder='Enter Username' className='pl-2 text-input ml-12 mt-1 w-60 h-10 outline-sky-50'/>
                   </div>
                 </div>
                 <div className="space-y-3">
                   <div className="form-control w-full flex flex-col">
-                    <label  className="label ml-12 caret-green-500 placeholder:ml-[5px]">
+                    <label  className="text-xs label ml-14 caret-green-500 placeholder:ml-[5px]">
                       Email
                     </label>
-                    <input type="text" value={signUpdata.email} onChange={(e)=>setSignupdata({...signUpdata, email: e.target.value})} required placeholder='Enter Email' className='text-input ml-12 mt-1 w-60 h-10 outline-sky-50'/>
+                    <input type="text" required placeholder='Enter Email' className='pl-2 text-input ml-12 mt-1 w-60 h-10 outline-sky-50'/>
                   </div>
                 </div>
                 <div className="space-y-3">
                   <div className="form-control w-full flex flex-col">
-                    <label  className="label ml-12 caret-green-500 placeholder:ml-[5px]">
+                    <label  className="text-xs label ml-14 caret-green-500 placeholder:ml-[5px]">
                       Password
                     </label>
-                    <input type="text" value={signUpdata.password} onChange={(e)=>setSignupdata({...signUpdata, password: e.target.value})} required placeholder='Enter Password' className='text-input ml-12 mt-1 w-60 h-10 outline-sky-50'/>
-                    <p className='ml-12 text-gray-500'>Join you friends and have fun!</p>
+                    <input type="password" required placeholder='Enter Password' className='pl-2 text-input ml-12 mt-1 w-60 h-10 outline-sky-50'/>
+                    <p className='text-xs ml-14 mt-1 text-gray-500'>Min. 6 characters</p>
                   </div>
+                </div>
+                <div className="space-y-2">
+                  <p className='ml-12 opacity-60 text-sm'>
+                    <input type="checkbox" name="" id="" className='mr-2' />
+                    I agree to the terms and conditions
+                  </p>
+                </div>
+                <div className="space-y-3 mt-5 pl-10 pr-20">
+                  <input type="submit" value="Register" className='w-full submit mr-25 h-10 text-center cursor-pointer b-r-left b-r-right'/>
+                </div>
+                <div className="space-y-2">
+                  <p className='ml-12'>Already have an account? <a href='/login' >Login</a></p>
                 </div>
               </div>
             </form>
           </div>
         </div>
-        <div className='h-140 w-2/4 b-r-right'>
+        <div className='w-2/4 h-140 b-r-right'>
           <figure className='p-10 mt-20'>
             <img  src='/images/Secure login-amico.svg' alt='signup-img'/>
           </figure>
